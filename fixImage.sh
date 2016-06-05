368608c7c4a30c15a621200f89c60dd3a4977ef8
ChinSyun Pang <pengzhanxuan@gmail.com>
2016-06-05 22:05:04 +0800

    fix image path

--- fixImage.sh
+++ fixImage.sh
@@ -6,6 +6,7 @@
 basePath=`echo "https://raw.githubusercontent.com/arthinking/arthinking.github.io/blog/source/_posts" | sed -e 's/\//\\\\\//g'`
 echo "base path: " $basePath
 postDir=/Users/arthinking/Dev/arthinking.github.io/source/_posts
+# postDir=~/Dev/test
 echo "post directory: " $postDir
 postLength=${#postDir}
 
@@ -25,14 +26,12 @@ traverseAllFiles() {
 	        dirPath=`echo $temp | awk -F '/[^/]*$' '{print $1}'`
 	        relativePath=`echo "${dirPath:$postLength}" | sed -e 's/\//\\\\\//g'`
 	        echo "=== 开始处理文件:"$relativePath
-	        # find "$dirPath/" -name "*.md" -print0 | xargs -0 perl -pi -e "s/(\!\[\]\((media.*?jpg)\))/\!\[\]\(${basePath}${relativePath}\/\2\)/g"
-	        find "$dirPath/" -name "*.md" -print0 | xargs -0 perl -pi -e "s/(media.*?jpg)/\!\[\]\(\2\)/g"
+	        find $dirPath -maxdepth 1 -name "*.md" -print0 | xargs -0 perl -pi -e "s/(\!\[\]\((media.*?jpg)\))/\!\[\]\(${basePath}${relativePath}\/\2\)/g"
+	        # find "$dirPath/" -name "*.md" -print0 | xargs -0 perl -pi -e "s/(media.*?jpg)/\!\[\]\(\2\)/g"
 	    fi
 	fi
     done
 }
-
-
 
 traverseAllFiles $postDir
 
